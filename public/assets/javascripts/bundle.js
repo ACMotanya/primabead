36caaa9a879000b698874500dd7605141f459301
Azubuike Motanya <acmotanya@gmail.com>
2017-02-09 16:09:21 -0500

    i guess we have 2 psychos in the house now

--- public/assets/javascripts/bundle.js
+++ public/assets/javascripts/bundle.js
@@ -9459,7 +9459,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /*jshint esversion: 6 */
+
 
 var Hello = function (_Component) {
   _inherits(Hello, _Component);
@@ -9476,7 +9477,7 @@ var Hello = function (_Component) {
       return _react2.default.createElement(
         'div',
         null,
-        'Hey, y\u2019all, It\'s ya boy!!'
+        'Hey, y\u2019all, Its ya boy!!'
       );
     }
   }]);
--- webpack/components/Hello.js
+++ webpack/components/Hello.js
@@ -1,8 +1,10 @@
+/*jshint esversion: 6 */
 import React, { Component } from 'react';
+
 class Hello extends Component {
  render() {
  return (
- <div>Hey, y’all, It's ya boy!!</div>
+ <div>Hey, y’all, Its ya boy!!</div>
  )
  }
 }
