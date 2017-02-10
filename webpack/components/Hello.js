d59897495ca64f9fcc2c1449f535e66314a5b408
Azubuike Motanya <acmotanya@gmail.com>
2017-02-10 09:32:13 -0500

    trying to fix this baseurl on gh-[apages

--- webpack/components/Hello.js
+++ webpack/components/Hello.js
@@ -2,10 +2,10 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
- render() {
- return (
- <div>Hey, y’all, Its ya boy!!</div>
- )
- }
+  render() {
+    return (
+      <div>Hey, y’all, Its ya boy!!</div>
+    )
+  }
 }
 export default Hello;
